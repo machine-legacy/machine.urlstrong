089b62333477d101dbe5333d064c2a4073e36609
Aaron Jensen <aaronjensen@gmail.com>
2009-01-31 14:28:23 -0800

    Adding some sample stuff.

---
+++ Source/Machine.RouteMap.Sample/Controllers/UserFriendController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machine.RouteMap.Sample.Controllers
+{
+  public class UserFriendController
+  {
+    public void List(Model model)
+    {
+      
+    }
+
+    public void Index(Model model)
+    {
+      
+    }
+  }
+}
