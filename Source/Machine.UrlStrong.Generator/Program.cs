abe2d5cdf577fc06397d8eb46e20323cee4c1ba5
Aaron Jensen <aaronjensen@gmail.com>
2009-03-07 15:25:06 -0800

    Adding urlstrong.exe project and rake open_sample to test.

---
+++ Source/Machine.UrlStrong.Generator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Machine.UrlStrong.Generator
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length < 2)
+      {
+        Console.WriteLine("usage: urlstrong.exe <url map> <output>");
+        Environment.Exit(1);
+        return;
+      }
+
+      if (!File.Exists(args[0]))
+      {
+        Console.WriteLine("Can't find url map: " + args[0]);
+        Environment.Exit(1);
+        return;
+      }
+
+      var outDir = Path.GetDirectoryName(args[1]);
+      if (!String.IsNullOrEmpty(outDir) && !Directory.Exists(outDir))
+      {
+        Console.WriteLine("Can't find output dir: " + outDir);
+        Environment.Exit(1);
+        return;
+      }
+
+      using(var reader = new StreamReader(args[0]))
+      using(var writer = new StreamWriter(args[1]))
+      {
+        var doIt = new DoIt();
+
+        doIt.Now(reader, writer);
+      }
+    }
+  }
+}
