57b86e9ff7ca81865ad0ebc69d770bfda5ebb8d1
Aaron Jensen <aaronjensen@gmail.com>
2009-03-07 16:27:57 -0800

    Figured out recursive templating, got classes creating.

--- Source/Machine.UrlStrong.Translation/Generation/TemplateBase.cs
+++ Source/Machine.UrlStrong.Translation/Generation/TemplateBase.cs
@@ -11,5 +11,32 @@ namespace Machine.UrlStrong.Translation.Generation
   public abstract class TemplateBase : AbstractSparkView
   {
     public UrlStrongModel Model { get; set; }
+
+    public string Indent(int tabs, string code)
+    {
+      var indentation = "  ";
+      for (int i = 0; i < tabs - 1; i++)
+      {
+        indentation += "  ";
+      }
+
+      var builder = new StringBuilder();
+      var reader = new StringReader(code);
+      var line = reader.ReadLine();
+
+      while (line != null)
+      {
+        builder.Append(indentation);
+        builder.AppendLine(line);
+        line = reader.ReadLine();
+      }
+
+      return builder.ToString();
+    }
+
+    public string Indent(string code)
+    {
+      return Indent(1, code);
+    }
   }
 }
