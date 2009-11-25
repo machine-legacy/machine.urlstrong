b5af31ef04f7b982f72971fa5405f0944668b385
Aaron Jensen <aaronjensen@gmail.com>
2009-11-24 19:56:46 -0800

    Adding a ToUrl method

--- Source/Machine.UrlStrong.Mvc/HtmlHelperExtensions.cs
+++ Source/Machine.UrlStrong.Mvc/HtmlHelperExtensions.cs
@@ -5,6 +5,14 @@ using System.Web.Mvc;
 
 namespace Machine.UrlStrong.Mvc
 {
+  public static class UrlExtensions
+  {
+    public static string ToUrl(this IUrl url)
+    {
+      return url.ToString();
+    }
+  }
+
   public static class HtmlHelperExtensions
   {
     public static string Url(this HtmlHelper htmlHelper, IUrl url)
